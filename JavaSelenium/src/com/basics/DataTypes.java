96d256d7a7e2ea05e25ead34d5e16aad8ea71f70
Mandula Naresh Babu <naresh2223@gmail.com>
2021-02-26 09:11:40 +0530

    26-Feb-2021:Naresh: Type Casting

--- JavaSelenium/src/com/basics/DataTypes.java
+++ JavaSelenium/src/com/basics/DataTypes.java
@@ -40,9 +40,46 @@ public class DataTypes {
 		boolean h = true;
 		System.out.println("Boolean value of h is : " + h);
 		
+		// Type Casting
+		/*
+		 * The process of converting data from one data type to another data type is as called as "Type Casting"
+		 * There are two types of type casting exists in Java
+		 * 1. Primitive data types Type Casting
+		 * 2. User Defined Data Types Type Casting
+		 * 
+		 * Note: To perform user defined data types Type Casting we need either "extends" relation or "implements" relationship
+		 * between user defined data types
+		 * 
+		 * 1. Primitive data types Type Casting
+		 * 
+		 * The process of converting one type of primitive data type to another type of primitive data type is called as Primitive
+		 * data type type casting
+		 * 
+		 * 
+		 * There are two types of primitive data type Type Casting
+		 * 
+		 * 1. Implicit (Widening) Type Casting : The process of converting data from lower data type to higher data type
+		 * 2. Explicit (Narrowing) Type Casting : The process of converting higher data type to lower data type
+		 * 		 * 
+		 * Implicit Type Casting : byte -> short -> int -> long -> float -> double
+		 * Explicit Type Casting : double -> float -> long -> -> int -> short -> byte
+		 * 
+		 * Type checking is the responsibility of compiler and Type Casting is the responsibility of JVM
+		 */
 		
-		// Narrowing and Widening casting
-
+		// Implicit or Widening or automatic type casting 
+		
+		byte a1 = 108;
+		int b1 = a1; // Assigning lower data type variable to a higher data type variable
+		System.out.println(b1);
+		long c1 =b1; // Assigning lower data type variable to a higher data type variable
+		System.out.println(c1);
+		
+		// Explicit or Narrowing type casting 
+		int d1 = 130;
+		byte e1 =(byte)d1; //  Assigning higher data type variable to a lower data type variable explicitly
+		System.out.println(e1);
+		
 	}
 
 }
